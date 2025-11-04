514c5df8d47ee78d35e61f4590002f7490b5c216
fonsfons28 <business.alfonsodolutan@gmail.com>
2025-11-04 14:39:58 +0800

    moved script.js to the same file level sa package.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db');
+const connectDB = require('./hospital-api/config/db');
 
 // Load environment variables
 dotenv.config();
@@ -16,8 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/doctors', require('./routes/doctorRoutes'));
-app.use('/api/patients', require('./routes/patientRoutes'));
+app.use('/api/doctors', require('./hospital-api/routes/doctorRoutes'));
+app.use('/api/patients', require('./hospital-api/routes/patientRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
